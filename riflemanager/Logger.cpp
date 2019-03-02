c49dec7aaace61036da814ef6f55397b4d5da375
Kenny McCormick <keennymccormic+github@gmail.com>
2019-03-02 16:45:57 -0600

    update Logger
    mostly to give it colours (terrible, not tested on other platforms)
    separated date functions (could be better?)

--- riflemanager/Logger.cpp
+++ riflemanager/Logger.cpp
@@ -3,18 +3,21 @@
 #include <string>
 #include <ctime>
 
-const std::string getDateTimestamp()
+#ifdef _WIN32
+	#include <Windows.h>
+	#undef ERROR
+#endif
+
+const std::string getDateStamp()
 {
 	time_t     now = time(0);
 	struct tm  timeinfo;
 	char       buf[80];
 	localtime_s(&timeinfo, &now);
-	strftime(buf, sizeof(buf), "%F.%H-%M-%S", &timeinfo);
+	strftime(buf, sizeof(buf), "%F", &timeinfo);
 
 	return buf;
 }
-
-std::string write_dir = "./rfman_log/" + getDateTimestamp() + ".log";
 
 const std::string getTimestamp()
 {
@@ -22,10 +25,12 @@ const std::string getTimestamp()
 	struct tm  timeinfo;
 	char       buf[80];
 	localtime_s(&timeinfo, &now);
-	strftime(buf, sizeof(buf), "%H:%M:%S", &timeinfo);
+	strftime(buf, sizeof(buf), "%H-%M-%S", &timeinfo);
 
 	return buf;
 }
+
+std::string write_dir = "./rfman_log/" + getDateStamp() + "." + getTimestamp() + ".log";
 
 void writeLog(const std::string output)
 {
@@ -44,7 +49,7 @@ void writeLog(const std::string output)
 	}
 	else
 	{
-		std::cerr << "/n" << "unable to open log file for writing!" << std::endl;
+		std::cerr << "unable to open log file for writing!" << std::endl;
 	}
 }
 
@@ -73,9 +78,20 @@ namespace logger
 	void WARNING(std::string output, bool silent)
 	{
 		output = getTimestamp() + " WARNING: " + output;
-		
+
 		if (!silent)
+		{
+#ifdef _WIN32 // TODO: make sure these work on other platforms
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 			std::cout << output << std::endl;
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+#elif // Unix probably
+			std::cout << "\u001b[33m" << output << "\u001b[0m" << std::endl;
+#else // anything else
+			std::cout << output << std::endl;
+#endif
+		}
 
 		writeLog(output);
 	}
@@ -85,7 +101,18 @@ namespace logger
 		output = getTimestamp() + " ERROR: " + output;
 		
 		if (!silent)
-			std::cerr << output << std::endl;
+		{
+#ifdef _WIN32 // TODO: make sure these work on other platforms
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+			std::cout << output << std::endl;
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+#elif // Unix probably
+			std::cout << "\u001b[31m" << output << "\u001b[0m" << std::endl;
+#else // anything else
+			std::cout << output << std::endl;
+#endif
+		}
 
 		writeLog(output);
 	}
